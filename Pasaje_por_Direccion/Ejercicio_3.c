6f7fb3714bcdf3e62a72fc90734d6d43b8f0cfb5
Nanu <nahuel.rd97@gmail.com>
2026-03-20 13:51:50 -0300

    Update Ejercicio_3.c

--- Pasaje_por_Direccion/Ejercicio_3.c
+++ Pasaje_por_Direccion/Ejercicio_3.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Función para calcular el máximo y mínimo implementando punteros.
+//Función para calcular máximo, mínimo y media implementando punteros.
 
-void maxmin (int x1, int x2, int * max, int * min){
+void maxmin_media (int x1, int x2, int * max, int * min, float * var_media);
+
+int main(){
+
+    int a=0, b=0, max=0, min=0;
+    float var_media;
+
+    printf("Ingrese dos numeros: \n");
+    scanf("%d %d", &a, &b);
+
+    maxmin_media(a, b, &max, &min, &var_media);
+    printf("Maximo: %d y Minimo: %d\n", max, min);
+    printf("La media entre ambos numeros es igual a %.2f\n", var_media);
+
+return 0;
+}
+
+void maxmin_media (int x1, int x2, int * max, int * min, float * var_media){
 
 if (x1 > x2) {
     *max = x1;
@@ -14,17 +31,6 @@ if (x1 > x2) {
         *min = x1;
     }
 
-}
+    *var_media = (float)(x1+x2)/2;
 
-int main(){
-
-    int a=0, b=0, max=0, min=0;
-
-    printf("Ingrese dos numeros: \n");
-    scanf("%d %d", &a, &b);
-
-    maxmin(a, b, &max, &min);
-    printf("El maximo entre %d y %d es %d. El minimo es %d\n", a, b, max, min);
-
-return 0;
 }
